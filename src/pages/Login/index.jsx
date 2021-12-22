5aa5f462cbf205d83c4d1628f8a47429b050806e
Bruno Lopes <eusoubruno7@gmail.com>
2021-12-22 17:23:50 -0300

    feat: creating login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Container, Content } from './styles';
+
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+
+import { Button, Container, Content, FooterForm } from './styles';
 
 import Input from '../components/Input';
 
@@ -7,9 +10,19 @@ const Login = function () {
   return (
     <Container>
       <Content>
-        <h1>LOGIN</h1>
-        <Input type="email" placeholder="Digite seu email" />
-        <Input type="password" placeholder="Digite sua senha" />
+        <h1>Never-forget</h1>
+        <h2>Faça seu Login</h2>
+        <form>
+          <Input type="email" placeholder="Digite seu email" />
+          <Input type="password" placeholder="Digite sua senha" />
+          <Button type="submit">Entrar</Button>
+        </form>
+        <FooterForm>
+          <MdKeyboardArrowLeft size={25} color="#504099" />
+          <a href="#">
+            <span>Cadastre-se!</span>
+          </a>
+        </FooterForm>
       </Content>
     </Container>
   );
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -10,10 +10,57 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  width: 31.25em;
+  max-width: 31.25em;
   height: 37.5em;
   background-color: var(--light-gray);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   > h1 {
+    color: var(--dark-blue);
+    cursor: default;
+  }
+  > h2 {
     color: var(--dark-gray);
+    cursor: default;
+  }
+  > form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3em 6.25em;
+  }
+  box-shadow: 10px 5px 5px black;
+`;
+
+export const Button = styled.button`
+  width: 15.625em;
+  height: 2.75em;
+  margin-top: 1.25em;
+  color: var(--dark-gray);
+  border: 0.125em solid var(--dark-gray);
+  border-radius: 0.3125em;
+  background: transparent;
+  align-self: flex-end;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  box-shadow: 10px 5px 5px black;
+  &:hover {
+    background: var(--dark-blue);
+    filter: brightness(1.5);
+  }
+`;
+
+export const FooterForm = styled.footer`
+  width: 18.75em;
+  display: flex;
+  align-items: center;
+  > a,
+  span {
+    text-decoration: none;
+    color: var(--dark-purple);
   }
 `;
