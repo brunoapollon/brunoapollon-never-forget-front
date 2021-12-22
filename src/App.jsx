12eccd12fd46d2b2996a3fa9f676c926052a5dc9
Bruno Lopes <eusoubruno7@gmail.com>
2021-12-22 15:10:09 -0300

    feat: creating global style

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyle from './styles/globalStyles';
 
 const App = function () {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 };
