b4bf943dc7cc1372d96eaac03cec4d2f0af87b98
Bruno Lopes <eusoubruno7@gmail.com>
2022-01-06 15:35:23 -0300

    feat: adding create account functionality to signup page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useCallback, createRef } from 'react';
 
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import api from '../../services/api';
 
 import { Button, Container, Content, FooterForm } from './styles';
 
 import Input from '../../components/Input';
 
 const SignUp = function () {
+  const inputEmailRef = createRef(null);
+  const inputNameRef = createRef(null);
+  const inputPasswordRef = createRef(null);
+
+  const handleSubmitCreateAccount = useCallback(async event => {
+    event.preventDefault();
+
+    const name = inputNameRef.current.value;
+    const email = inputEmailRef.current.value;
+    const password = inputPasswordRef.current.value;
+
+    if (name.trim() === '' || email.trim() === '' || password.trim() === '') {
+      return;
+    }
+
+    await api.post('users/', { name, email, password });
+    inputNameRef.current.value = '';
+    inputEmailRef.current.value = '';
+    inputPasswordRef.current.value = '';
+  }, []);
+
   return (
     <Container>
       <Content>
         <h1>Never-forget</h1>
         <h2>Cadastre-se e não se esqueça de nada!</h2>
-        <form>
-          <Input type="text" placeholder="Digite seu nome" />
-          <Input type="email" placeholder="Digite seu email" />
-          <Input type="password" placeholder="Digite sua senha" />
+        <form onSubmit={event => handleSubmitCreateAccount(event)}>
+          <Input type="text" placeholder="Digite seu nome" ref={inputNameRef} />
+          <Input
+            type="email"
+            placeholder="Digite seu email"
+            ref={inputEmailRef}
+          />
+          <Input
+            type="password"
+            placeholder="Digite sua senha"
+            ref={inputPasswordRef}
+          />
           <Button type="submit">Criar conta</Button>
         </form>
         <FooterForm>
